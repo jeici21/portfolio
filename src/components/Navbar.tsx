da94dd29b51d67e2d27d423a4ab4504d12a917e0
Jorge Castro <david92_jc@yahoo.es>
2023-07-28 16:01:20 -0500

    creada Portada

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,26 +19,26 @@ const Navbar = () => {
                     <i className="bi bi-list text-light" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 fst-italic">
+                        <li className="nav-item ps-5">
                             <a className={`nav-link text-light ${active === 0 && 'fw-bold'}`}
                                 onClick={() => setActive(0)} href="#">
                                 <Span>Sobre mí</Span>
                             </a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item ps-5">
                             <a className={`nav-link text-light ${active === 1 && 'fw-bold'}`}
                                 onClick={() => setActive(1)} href="#">
                                 <Span>Formación y experiencia</Span>
                             </a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item ps-5">
                             <a className={`nav-link text-light ${active === 2 && 'fw-bold'}`}
                                 onClick={() => setActive(2)} href="#">
                                 <Span>Aptitudes</Span>
                             </a>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item ps-5">
                             <a className={`nav-link text-light ${active === 3 && 'fw-bold'}`}
                                 onClick={() => setActive(3)} href="#">
                                 <Span>Proyectos</Span>
--- src/components/Portada.tsx
+++ src/components/Portada.tsx
@@ -1,18 +1,37 @@
 import styled from 'styled-components'
 import fondo from '../assets/fondo.jpg'
+import yo from '../assets/yo.jpg'
 
 const Div = styled.div`
-    background: url(${fondo});
+    background: url(${fondo}) 100%;
 `
 
 const Portada = () => {
     return (
-        <div className="row">
-            <Div className="col-9">
-                <p>HOLA, SOY</p>
-                <h1>Jorge Castro</h1>
-                <p>Ingeniero en Sistemas Computacionales</p>
-            </Div>
+        <div className='container-fluid'>
+            <div className="row">
+                <Div className="col-9 text-white">
+                    <p>HOLA, SOY</p>
+                    <h1>Jorge Castro</h1>
+                    <p>Ingeniero en Sistemas Computacionales</p>
+                </Div>
+                <div className="col text-bg-primary text-center">
+                    <img src={yo} alt="Yo" width="150" height='150' className='rounded-circle' />
+                    <h1 className='fw-bold'>Jorge Castro</h1>
+                    <p className='py-4'>Ingeniero en Sistemas Computacionales. <br/>Universidad Estatal de Milagro</p>
+                    <a href="mailto:david92_jc@yahoo.es" className='link-light link-underline-opacity-0 link-underline-opacity-100-hover'>
+                        david92_jc@yahoo.es
+                    </a>
+                    <p>Tel: 914-123-456</p>
+                    <div className='row px-5'>
+                        <a href="https://www.linkedin.com/in/jorge-castro-vargas-7242a8129/" target='_blank' className='col bi bi-linkedin text-white' />
+                        <a href="https://www.facebook.com/jeici21/" target='_blank' className='col bi bi-facebook text-white' />
+                        <a href="https://twitter.com/cijei21" target='_blank' className='col bi bi-twitter text-white' />
+                        <a href="https://github.com/jeici21" target='_blank' className='col bi bi-github text-white' />
+                    </div>
+                    <button className='btn btn-light btn-lg my-5'>Descargar CV</button>
+                </div>
+            </div>
         </div>
     )
 }
