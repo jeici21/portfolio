ee9a6f246e6401519953dcacffbd0c8961f26626
Jorge Castro <david92_jc@yahoo.es>
2023-07-29 17:32:04 -0500

    correcciones menores

--- src/components/Portada.tsx
+++ src/components/Portada.tsx
@@ -25,7 +25,7 @@ const Portada = () => {
                         david92_jc@yahoo.es
                     </a>
                     <p>Tel: 914-123-456</p>
-                    <div className='row px-5'>
+                    <div className='row px-5 fs-5'>
                         <a href="https://www.linkedin.com/in/jorge-castro-vargas-7242a8129/" target='_blank' className='col bi bi-linkedin text-white' />
                         <a href="https://www.facebook.com/jeici21/" target='_blank' className='col bi bi-facebook text-white' />
                         <a href="https://twitter.com/cijei21" target='_blank' className='col bi bi-twitter text-white' />
