faebb09709e125e461249e109107fbefe00671af
Jorge Castro <david92_jc@yahoo.es>
2023-08-01 17:49:31 -0500

    correcciones menores

--- src/components/Portada.tsx
+++ src/components/Portada.tsx
@@ -1,19 +1,13 @@
 import styled from 'styled-components'
 import fondo from '../assets/fondo.jpg'
 import yo from '../assets/yo.jpeg'
+import CV from '/Jorge_Castro_Vargas.pdf'
 
 const Div = styled.div`
     background: linear-gradient(rgba(1, 33, 82, 0.5), rgba(1, 33, 82, 0.5)), url(${fondo}) fixed;
 `;
 
 const Portada = () => {
-    const handleDownloadCV = () => {
-        const downloadLink = document.createElement('a');
-        downloadLink.href = '/Jorge_Castro_Vargas.pdf';
-        downloadLink.download = 'Jorge Castro Vargas.pdf';
-        downloadLink.click();
-    };
-
     return (
         <div className='container-fluid'>
             <div className="row">
@@ -27,19 +21,24 @@ const Portada = () => {
                 <div className="col text-bg-primary text-center">
                     <img src={yo} alt="Yo" width="150" height='150' className='rounded-circle' />
                     <h1 className='fw-bold'>Jorge Castro</h1>
-                    <p className='py-4'>Ingeniero en Sistemas Computacionales. <br/>Universidad Estatal de Milagro</p>
+                    <p className='py-4'>Ingeniero en Sistemas Computacionales. <br />Universidad Estatal de Milagro</p>
                     <a href="mailto:david92_jc@yahoo.es" className='link-light link-underline-opacity-0 link-underline-opacity-100-hover'>
                         david92_jc@yahoo.es
                     </a>
                     <p>Tel: +593997543298</p>
                     <div className='row px-5 fs-5'>
-                        <a href="https://www.linkedin.com/in/jorge-castro-vargas-7242a8129/" target='_blank' className='col bi bi-linkedin text-white' />
-                        <a href="https://www.facebook.com/jeici21/" target='_blank' className='col bi bi-facebook text-white' />
-                        <a href="https://twitter.com/cijei21" target='_blank' className='col bi bi-twitter text-white' />
-                        <a href="https://github.com/jeici21" target='_blank' className='col bi bi-github text-white' />
+                        <a href="https://www.linkedin.com/in/jorge-castro-vargas-7242a8129/"
+                            rel="noopener noreferrer" target='_blank' className='col bi bi-linkedin text-white' />
+                        <a href="https://www.facebook.com/jeici21/" target='_blank'
+                            rel="noopener noreferrer" className='col bi bi-facebook text-white' />
+                        <a href="https://twitter.com/cijei21" target='_blank'
+                            rel="noopener noreferrer" className='col bi bi-twitter text-white' />
+                        <a href="https://github.com/jeici21" target='_blank'
+                            rel="noopener noreferrer" className='col bi bi-github text-white' />
                     </div>
-                    <button onClick={handleDownloadCV} className='btn btn-light btn-lg my-5 rounded-0'>
-                        Descargar CV
+                    <button onClick={() => window.open(CV, '_blank')}
+                        className='btn btn-light btn-lg my-5 rounded-0' aria-label="Descargar CV">
+                        <i className="bi bi-download" /> Descargar CV
                     </button>
                 </div>
             </div>
