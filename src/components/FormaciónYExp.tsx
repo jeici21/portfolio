2dfa31e92fbf8a53e81912729056fbbb3ce2377f
Jorge Castro <david92_jc@yahoo.es>
2023-08-02 14:54:25 -0500

    Añadida Formación y Experiencia

--- src/components/FormaciónYExp.tsx
+++ src/components/FormaciónYExp.tsx
@@ -1,6 +1,29 @@
 const FormaciónYExp = () => {
     return (
-        <div id="formacion-y-experiencia">FormaciónYExp</div>
+        <div id="formacion-y-experiencia" className="container pt-5">
+            <div className="row">
+                <div className="col">
+                    <h2 className="fst-italic text-primary">Formación</h2>
+                    <p className="text-primary">MIS CONOCIMIENTOS</p>
+                    <h5 className="pt-5">2004 - 2010</h5>
+                    <p>Graduado en la especialidad de Informática</p>
+                    <p>Colegio Fiscal Econ. Abdón Calderón Muñoz</p>
+                    <h5 className="pt-5">2010 - 2016</h5>
+                    <p>Graduado con el título de Ingeniero en Sistemas Computacionales</p>
+                    <p>Universidad Estatal de Milagro</p>
+                </div>
+                <div className="col">
+                    <h2 className="fst-italic text-primary">Experiencia</h2>
+                    <p className="text-primary">TRAYECTORIA PROFESIONAL</p>
+                    <h5 className="pt-5">Junio 2023-Actualidad</h5>
+                    <p>EICHE, Web Developer Vue-React</p>
+                    <h5 className="pt-5">Noviembre 2022-Febrero 2023</h5>
+                    <p>Kruger Corporation, Ex participante de Kruger Star</p>
+                    <h5 className="pt-5">Septiembre 2020-Noviembre 2020</h5>
+                    <p>GYSECOMP, Web Developer</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
