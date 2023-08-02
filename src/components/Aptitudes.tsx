189fcf185ba88dea55e8739fa3ccfd06419bbe66
Jorge Castro <david92_jc@yahoo.es>
2023-08-02 16:16:44 -0500

    Añadidas aptitudes

--- src/components/Aptitudes.tsx
+++ src/components/Aptitudes.tsx
@@ -1,6 +1,35 @@
 const Aptitudes = () => {
+    const aptitudes = [
+        { nombre: 'Análisis de datos', valor: 50 },
+        { nombre: 'Redacción de contenidos', valor: 75 },
+        { nombre: 'HTML y CSS', valor: 100 },
+        { nombre: 'Javascript', valor: 100 },
+        { nombre: 'Typescript', valor: 75 },
+        { nombre: 'React', valor: 75 },
+        { nombre: 'Nodejs', valor: 75 },
+        { nombre: 'Vue', valor: 50 },
+    ]
+
     return (
-        <div id="aptitudes">Aptitudes</div>
+        <div id="aptitudes" className="container pt-5">
+            <h2 className="fst-italic text-primary">Aptitudes</h2>
+            <p className="text-primary">LO QUE PUEDO APORTAR</p>
+            <table className="table table-borderless">
+                <tbody>
+                    {aptitudes.map((aptitud, index) => (
+                        <tr key={index}>
+                            <td className="col-4">{aptitud.nombre}</td>
+                            <td className="col">
+                                <div className="progress" role="progressbar" aria-label={aptitud.nombre}
+                                    aria-valuenow={aptitud.valor} aria-valuemin={0} aria-valuemax={100}>
+                                    <div className={`progress-bar progress-bar-striped progress-bar-animated w-${aptitud.valor}`} />
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
