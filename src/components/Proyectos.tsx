bd61088c797174e1044b83f3c30a7d3dec3aaef5
Jorge Castro <david92_jc@yahoo.es>
2023-08-03 17:30:09 -0500

    Añadidas secciones de proyectos e intereses

--- src/components/Proyectos.tsx
+++ src/components/Proyectos.tsx
@@ -1,6 +1,36 @@
 const Proyectos = () => {
+    const proyectos = [{
+        img: "https://raw.githubusercontent.com/jeici21/proyecto-final/ProductPage/src/KMarket.png",
+        titulo: 'KMarket Ecommerce',
+        descripcion: 'Un sitio web para un supermercado con funciones de agregar, modificar y eliminar productos junto a sus respectivas categorías. También es posible revisar más detalles sobre los productos y agregarlos al carrito de compras.',
+        repositorio: 'https://github.com/jeici21/proyecto-final',
+        link: ''
+    }]
+
     return (
-        <div id="proyectos">Proyectos</div>
+        <div id="proyectos" className="container text-center pt-5">
+            <h2 className="fst-italic text-primary">Mis proyectos</h2>
+            <p className="text-primary">MIS TRABAJOS HASTA EL MOMENTO</p>
+            <div className="row row-cols-3">
+                {proyectos.map((proyecto, index) => (
+                    <div className="col" key={index}>
+                        <div className="card">
+                            <img src={proyecto.img} className="card-img-top" alt="K-Market E-commerce" />
+                            <div className="card-body">
+                                <h5 className="card-title">{proyecto.titulo}</h5>
+                                <p className="card-text">{proyecto.descripcion}</p>
+                                {proyecto.repositorio && <a href={proyecto.repositorio} className="btn btn-success mx-1" target="_blank">
+                                    Ver repositorio
+                                </a>}
+                                {proyecto.link && <a href={proyecto.link} className="btn btn-danger mx-1" target="_blank">
+                                    Visitar página
+                                </a>}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
--- src/components/Sobremi.tsx
+++ src/components/Sobremi.tsx
@@ -2,12 +2,27 @@ const SobreMi = () => {
     return (
         <div className="bg-primary bg-opacity-10 pt-5" id="sobre-mi">
             <div className="container">
-                <h2 className="fst-italic text-primary">Sobre mí</h2>
-                <p className="text-primary">MI PERFIL</p>
-                <h4 className="py-5">Me apasionan la programación y el diseño de páginas web, y poseo las aptitudes y
-                    experiencia profesional necesarias para entrar en dicho mundo. Como un profesional
-                    optimista con excelentes habilidades de trabajo en equipo, espero unfuturo brillante
-                    en esta industria. Explora mi sitio para ver lo que puedo aportar.</h4>
+                <div className="row">
+                    <div className="col">
+                        <h2 className="fst-italic text-primary">Sobre mí</h2>
+                        <p className="text-primary">MI PERFIL</p>
+                        <h4 className="py-5">Me apasionan la programación y el diseño de páginas web, y poseo las aptitudes y
+                            experiencia profesional necesarias para entrar en dicho mundo. Como un profesional
+                            optimista con excelentes habilidades de trabajo en equipo, espero unfuturo brillante
+                            en esta industria. Explora mi sitio para ver lo que puedo aportar.</h4>
+                    </div>
+                    <div className="col">
+                        <h2 className="fst-italic text-primary">Intereses</h2>
+                        <p className="text-primary">EN MIS RATOS LIBRES</p>
+                        <div className="row row-cols-2 pt-5">
+                            <p className="col">🎮 Videojuegos</p>
+                            <p className="col">💻 Programación</p>
+                            <p className="col">📺 Series</p>
+                            <p className="col">⚽ Fútbol</p>
+                            <p className="col">📖 Lectura</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
