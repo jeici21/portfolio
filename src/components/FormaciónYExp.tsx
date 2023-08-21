daaa56a98522ef13390112b54c2941769edf5b6a
Jorge Castro <david92_jc@yahoo.es>
2023-08-20 22:32:12 -0500

    añadida transición a FormaciónYExp

--- src/components/FormaciónYExp.tsx
+++ src/components/FormaciónYExp.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useRef, useState } from "react"
+import { styled } from "styled-components"
+
+const Div = styled.div<{ visibility: string }>`
+    opacity: ${props => props.visibility === 'true' ? 1 : 0};
+    transform: ${props => props.visibility === 'true' ? 'none' : 'translateY(10vh)'};
+    visibility: ${props => props.visibility === 'true' ? 'visible' : 'hidden'};
+    transition: opacity 0.6s ease-out, transform 1.2s ease-out;
+    will-change: opacity, visibility;
+`
+
 const FormaciónYExp = () => {
+    const [visible, setVisible] = useState(true)
+    const domRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => setVisible(entry.isIntersecting));
+        });
+        observer.observe(domRef.current!);
+        return () => observer.unobserve(domRef.current!);
+    }, [])
+
     return (
         <div id="formacion-y-experiencia" className="container pt-5">
-            <div className="row">
+            <Div visibility={String(visible)} className="row" ref={domRef}>
                 <div className="col-md-6">
                     <h2 className="fst-italic text-primary text-center text-md-start">Formación</h2>
                     <p className="text-primary text-center text-md-start">MIS CONOCIMIENTOS</p>
@@ -19,7 +41,7 @@ const FormaciónYExp = () => {
                     <p className="pt-5 fw-bold">Septiembre 2020 - Noviembre 2020</p>
                     <p>GYSECOMP, Web Developer</p>
                 </div>
-            </div>
+            </Div>
         </div>
     )
 }
