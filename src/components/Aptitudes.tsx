3c3967ee42c4b42645c7176223aa276548f75df7
Jorge Castro <david92_jc@yahoo.es>
2023-08-28 15:32:53 -0500

    Añadidas nuevas aptitudes

--- src/components/Aptitudes.tsx
+++ src/components/Aptitudes.tsx
@@ -27,6 +27,8 @@ const Aptitudes = () => {
         { icono: "https://img.icons8.com/?size=1x&id=hsPbhkOH4FMe&format=png", nombre: 'Nodejs', valor: 75 },
         { icono: "https://img.icons8.com/?size=1x&id=kg46nzoJrmTR&format=png", nombre: 'Express', valor: 75 },
         { icono: "https://img.icons8.com/?size=512&id=HKNzD81eiiSc&format=png", nombre: 'jQuery', valor: 70 },
+        { icono: "https://img.icons8.com/?size=512&id=84710&format=png", nombre: 'Bootstrap', valor: 90 },
+        { icono: "https://img.icons8.com/?size=512&id=4PiNHtUJVbLs&format=png", nombre: 'Tailwind', valor: 75 },
     ]
 
     useEffect(() => {
