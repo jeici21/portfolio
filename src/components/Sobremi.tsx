0c6bdd92b7296fa6ac36ab5e0b6625f6b1bc2f23
Jorge Castro <david92_jc@yahoo.es>
2023-09-01 17:34:24 -0500

    Correción ortográfica

--- src/components/Sobremi.tsx
+++ src/components/Sobremi.tsx
@@ -23,7 +23,7 @@ const SobreMi = () => {
                         <p className="text-primary text-center text-md-start">MI PERFIL</p>
                         <p className="pt-5 fs-5 lh-lg">Me apasionan la programación y el diseño de páginas web, y poseo las aptitudes y
                             experiencia profesional necesarias para entrar en dicho mundo. Como un profesional
-                            optimista con excelentes habilidades de trabajo en equipo, espero unfuturo brillante
+                            optimista con excelentes habilidades de trabajo en equipo, espero un futuro brillante
                             en esta industria. Explora mi sitio para ver lo que puedo aportar.</p>
                     </div>
                     <div className="col-md-6 pt-5 pt-md-0">
